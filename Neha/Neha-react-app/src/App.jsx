2d154e7a794bfedd17dc0fc2adbfb640591f7be2
NehaAhen25 <neha04102005@gmail.com>
2026-07-30 09:54:34 +0530

    fix the changes

--- Neha/Neha-react-app/src/App.jsx
+++ Neha/Neha-react-app/src/App.jsx
@@ -1,7 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import { MdAutoDelete } from "react-icons/md";
 import { FaCircleCheck } from "react-icons/fa6";
@@ -26,12 +23,14 @@ function App() {
     setNewDescription("");
     localStorage.setItem('todolist',JSON.stringify(updateTodoArr));
   };
+
   const handleDeleteTodo = (index)=>{
     let reducedTodo = [...allTodos];
-    reducedTodo.splice(index);
+    reducedTodo.splice(index, 1);   
     localStorage.setItem('todolist',JSON.stringify(reducedTodo));
     setTodos(reducedTodo)
-  } 
+  }
+
   const handleComplete = (index)=>{
     let now = new Date();
     let dd =now.getDate();
@@ -50,67 +49,98 @@ function App() {
     updateCompletedArr.push(filterItem);
     setcompletedTodos(updateCompletedArr);
     handleDeleteTodo(index);
-    localStorage.setItem('completedTodos',JSON.stringify(updateTodoArr));
+
+    
+    localStorage.setItem('completedTodos',JSON.stringify(updateCompletedArr));
   }
+
   const handleDeleteCompletedTodo  = (index)=>{
     let reducedTodo = [...completedTodos];
-    reducedTodo.splice(index);
+    reducedTodo.splice(index, 1);  
 
-    localStorage.setItem('CompletedTodos',JSON.stringify(reducedTodo));
+    
+    localStorage.setItem('completedTodos',JSON.stringify(reducedTodo));
     setcompletedTodos(reducedTodo)
   }
+
   useEffect(()=>{
     let savedTodo = JSON.parse(localStorage.getItem('todolist'));
-    let savedCompletedTodo = JSON.parse(localStorage.getItem('comletedTodo'));
+
+    
+    let savedCompletedTodo = JSON.parse(localStorage.getItem('completedTodos'));
+
     if(savedTodo){
       setTodos(savedTodo);
     }
     if(savedCompletedTodo){
       setcompletedTodos(savedCompletedTodo);
-      
+
     }
 
-         },[])
+  },[])
 
   return (
     <>
       <section id="App">
         <div>
           <h1>MY TODO LIST</h1>
- {/* paragraph below my todo list */}
           <p>
-            Add your tasks and manage your daily activities effectively ! </p>
+            Add your tasks and manage your daily activities effectively !
+          </p>
         </div>
       </section>
 
-
       <section id="next-steps">
         <div id="docs">
-          
+
           <div className='todo-wrapper'>
             <div className='todo-input'>
               <div className='todo-input-item'>
-              <label >Title</label>
-              <input type="text" value={newtitle} onChange={(e)=>setNewTitle(e.target.value)} placeholder='Enter your task title' />
-            </div>
-            <div className='todo-input-item'>
-              <label >Description</label>
-              <input type="text" value={newDescription} onChange={(e)=>setNewDescription(e.target.value)} placeholder='Enter your task Description' />
-            </div>
-            <div className='todo-input-item'>
-              <label ></label>
-              <button type="button" onClick={handleAddTodo} className='primarybtn'>Add Task</button>
-            </div>
+                <label>Title</label>
+                <input
+                  type="text"
+                  value={newtitle}
+                  onChange={(e)=>setNewTitle(e.target.value)}
+                  placeholder='Enter your task title'
+                />
+              </div>
+
+              <div className='todo-input-item'>
+                <label>Description</label>
+                <input
+                  type="text"
+                  value={newDescription}
+                  onChange={(e)=>setNewDescription(e.target.value)}
+                  placeholder='Enter your task Description'
+                />
+              </div>
+
+              <div className='todo-input-item'>
+                <label></label>
+                <button
+                  type="button"
+                  onClick={handleAddTodo}
+                  className='primarybtn'
+                >
+                  Add Task
+                </button>
+              </div>
             </div>
 
             <div className='btn-area'>
-              <button className={`secondrybtn ${viewMode==='todo' ? 'active' : ''}`}
+              <button
+                className={`secondrybtn ${viewMode==='todo' ? 'active' : ''}`}
                 onClick={()=>setViewMode('todo')}
-              >TODO</button>
-              
-              <button className={`secondrybtn ${viewMode==='completed' ? 'active' : ''}`}
+              >
+                TODO
+              </button>
+
+              <button
+                className={`secondrybtn ${viewMode==='completed' ? 'active' : ''}`}
                 onClick={()=>setViewMode('completed')}
-              >COMPLETED</button>
+              >
+                COMPLETED
+              </button>
             </div>
 
             <div className='todo-list'>
@@ -121,8 +151,16 @@ function App() {
                     <p>{item.description}</p>
                   </div>
                   <div>
-                    <MdAutoDelete className='icon' onClick={()=>handleDeleteTodo(index)} title='Delete?'/>
-                    <FaCircleCheck className='check-icon' onClick={()=>handleComplete(index)} title='Complete?'/>
+                    <MdAutoDelete
+                      className='icon'
+                      onClick={()=>handleDeleteTodo(index)}
+                      title='Delete?'
+                    />
+                    <FaCircleCheck
+                      className='check-icon'
+                      onClick={()=>handleComplete(index)}
+                      title='Complete?'
+                    />
                   </div>
                 </div>
               ))}
@@ -135,7 +173,11 @@ function App() {
                     <p><small>Completed on: {item.completeOn}</small></p>
                   </div>
                   <div>
-                    <MdAutoDelete className='icon' onClick={()=>handleDeleteCompletedTodo(index)} title='Delete?'/>
+                    <MdAutoDelete
+                      className='icon'
+                      onClick={()=>handleDeleteCompletedTodo(index)}
+                      title='Delete?'
+                    />
                   </div>
                 </div>
               ))}
@@ -143,11 +185,6 @@ function App() {
           </div>
         </div>
       </section>
-        
-      
-
-      
-      
     </>
   )
 }
